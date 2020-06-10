56cf0b00d48764159874d9655301920fd2ad93f8
nekowen <nekonyanowen@gmail.com>
2020-06-10 11:45:58 +0900

    Add sample code

---
+++ UILabel-Color-Sample/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  UILabel-Color-Sample
+//
+//  Created by nekowen on 2020/06/10.
+//  Copyright © 2020 nekowen. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
+
---
+++ UILabel-Color-Sample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UILabel-Color-Sample
+//
+//  Created by nekowen on 2020/06/10.
+//  Copyright © 2020 nekowen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
